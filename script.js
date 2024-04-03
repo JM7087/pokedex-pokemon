575e00fd6503f6c5d2dab2bdd796be30418bf6e4
João Marcos de Oliveira santos <joaomarcos@jm7087.com>
2024-04-03 10:59:59 -0300

    adicionando Web Speech API no projeto

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function buscarPokemon() {
 
   // Função para exibir os detalhes do Pokémon
   function exibirPokemon(pokemonData) {
-    const { name, height, weight, types, id, sprites, base_experience, abilities} = pokemonData;
+    const { name, height, weight, types, id, sprites, base_experience, abilities } = pokemonData;
 
     const abilitiesNames = abilities.map(ability => ability.ability.name).join(', ')
 
@@ -37,7 +37,7 @@ function buscarPokemon() {
       pokemonData.sprites.versions["generation-v"]["black-white"].animated.front_default;
 
     if (imagemGif == null) {
-       PokemonImagem = sprites.front_default;
+      PokemonImagem = sprites.front_default;
 
     }
     else {
@@ -47,9 +47,9 @@ function buscarPokemon() {
     // para converter Decimetros é Hectogramas em Metros é Kg 
     // é só dividir o valor de height e weight por 10
 
-    const altura = TrocarPontoPorVirgula(height / 10) 
+    const altura = TrocarPontoPorVirgula(height / 10)
 
-    const peso = TrocarPontoPorVirgula(weight / 10) 
+    const peso = TrocarPontoPorVirgula(weight / 10)
 
 
     const resultDiv = document.getElementById("result");
@@ -59,17 +59,44 @@ function buscarPokemon() {
           <p><strong>Altura:</strong> ${altura} Metros</p>
           <p><strong>Peso:</strong> ${peso} Kg</p>
           <p><strong>Nivel Base de Experiência:</strong> ${base_experience}</p>
-          <p><strong>Habilidades:</strong> ${abilitiesNames}</p>
+          <p><strong>Habilidades:</strong> ${abilitiesNames}.</p>
           <p><strong>Tipo:</strong> ${types.map((type) => type.type.name).join(", ")}</p>
           <p><strong>Numero:</strong> ${id}</p>
           <!-- Adicione outras informações que desejar -->
         `;
+
+    let dadosDoPokemon = `
+          ${name}
+          Altura ${altura} Metros
+          Peso ${peso} Kg
+          Nivel Base de Experiência ${base_experience}
+          Habilidades ${abilitiesNames}
+          Tipo ${types.map((type) => type.type.name).join(", ")}
+          Numero na Pokedex ${id}
+        `
+
+    falarTexto(dadosDoPokemon);
   }
 }
 
 function TrocarPontoPorVirgula(valor) {
   return valor.toString().replace(".", ",")
 }
+
+function falarTexto(dadosDoPokemon) {
+
+  // Verifica se o navegador suporta a Web Speech API
+  if ('speechSynthesis' in window) {    
+    // Cria um novo objeto SpeechSynthesisUtterance para definir o texto a ser falado
+    let falar = new SpeechSynthesisUtterance(dadosDoPokemon);
+
+    // Fala o texto
+    window.speechSynthesis.speak(falar);
+  } else {
+    console.log('Seu navegador não suporta a Web Speech API. Tente um navegador mais recente.');
+  }
+}
+
 
 // Ouvinte de evento para o botão "Buscar"
 document
