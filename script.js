b5427bfa867e7607a6a0d70d6e9aecfafbb76375
JM7087 <joaomarcos@jm7087.com>
2024-08-09 20:43:53 -0300

    melhorias na interface

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Função para buscar o Pokémon
 function buscarPokemon() {
   let pokemonNameOuNumero = document
     .getElementById("characterName")
@@ -7,6 +6,9 @@ function buscarPokemon() {
   const resultDiv = document.getElementById("result");
 
   if (pokemonNameOuNumero.trim() !== "") {
+
+    // Remove o display: none antes de começar a exibir os resultados
+    resultDiv.style.display = "block";
 
     resultDiv.innerHTML = `
         <div>
@@ -25,32 +27,24 @@ function buscarPokemon() {
       });
   }
 
-  // Função para exibir os detalhes do Pokémon
   function exibirPokemon(pokemonData) {
     const { name, height, weight, types, id, sprites, base_experience, abilities } = pokemonData;
 
-    const abilitiesNames = abilities.map(ability => ability.ability.name).join(', ')
+    const abilitiesNames = abilities.map(ability => ability.ability.name).join(', ');
 
-    let PokemonImagem
+    let PokemonImagem;
 
     const imagemGif =
       pokemonData.sprites.versions["generation-v"]["black-white"].animated.front_default;
 
     if (imagemGif == null) {
       PokemonImagem = sprites.front_default;
-
-    }
-    else {
-      PokemonImagem = imagemGif
+    } else {
+      PokemonImagem = imagemGif;
     }
 
-    // para converter Decimetros é Hectogramas em Metros é Kg 
-    // é só dividir o valor de height e weight por 10
-
-    const altura = TrocarPontoPorVirgula(height / 10)
-
-    const peso = TrocarPontoPorVirgula(weight / 10)
-
+    const altura = TrocarPontoPorVirgula(height / 10);
+    const peso = TrocarPontoPorVirgula(weight / 10);
 
     const resultDiv = document.getElementById("result");
     resultDiv.innerHTML = `
@@ -62,7 +56,6 @@ function buscarPokemon() {
           <p><strong>Habilidades:</strong> ${abilitiesNames}.</p>
           <p><strong>Tipo:</strong> ${types.map((type) => type.type.name).join(", ")}</p>
           <p><strong>Numero:</strong> ${id}</p>
-          <!-- Adicione outras informações que desejar -->
         `;
 
     let dadosDoPokemon = `
@@ -73,37 +66,29 @@ function buscarPokemon() {
           Habilidades ${abilitiesNames}
           Tipo ${types.map((type) => type.type.name).join(", ")}
           Numero na Pokedex ${id}
-        `
+        `;
 
     falarTexto(dadosDoPokemon);
   }
 }
 
 function TrocarPontoPorVirgula(valor) {
-  return valor.toString().replace(".", ",")
+  return valor.toString().replace(".", ",");
 }
 
 function falarTexto(dadosDoPokemon) {
-
-  // Verifica se o navegador suporta a Web Speech API
-  if ('speechSynthesis' in window) {    
-    // Cria um novo objeto SpeechSynthesisUtterance para definir o texto a ser falado
+  if ('speechSynthesis' in window) {
     let falar = new SpeechSynthesisUtterance(dadosDoPokemon);
-
-    // Fala o texto
     window.speechSynthesis.speak(falar);
   } else {
     console.log('Seu navegador não suporta a Web Speech API. Tente um navegador mais recente.');
   }
 }
 
-
-// Ouvinte de evento para o botão "Buscar"
 document
   .querySelector(".search-container button")
   .addEventListener("click", buscarPokemon);
 
-// Ouvinte de evento para a tecla "Enter"
 document
   .getElementById("characterName")
   .addEventListener("keypress", function (event) {
